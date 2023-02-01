2e7ea466df8db743fefd3f9e1454408de3ae51de
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-02-01 22:09:00 +0100

    Add some part of the dynamic popup.

--- main.js
+++ main.js
@@ -33,3 +33,47 @@ detailCardOff.addEventListener('click', () => {
   cardDetails.classList.toggle('full-details-show');
 });
 // Details popup ends here
+
+const projects = [{
+  projectName: 'Professional Art Printing Data',
+  projectLang: ['html', 'bootstrap', 'Ruby'],
+  featureImg: './bd-desk-one.png',
+  projectDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+  seeLive: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+  seeSource: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+}, {
+  projectName: 'Data Dashboard Healthcare',
+  projectLang: ['html', 'bootstrap', 'Ruby'],
+  featureImg: './bg-dash.png',
+  projectDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+  seeLive: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+  seeSource: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+}, {
+  projectName: 'Website Portfolio',
+  projectLang: ['html', 'bootstrap', 'Ruby'],
+  featureImg: './bg-web.png',
+  projectDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+  seeLive: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+  seeSource: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+}, {
+  projectName: 'Professional Art Printing Data',
+  projectLang: ['html', 'bootstrap', 'Ruby'],
+  featureImg: './bd-desk-one.png',
+  projectDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+  seeLive: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+  seeSource: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+}, {
+  projectName: 'Data Dashboard Healthcare',
+  projectLang: ['html', 'bootstrap', 'Ruby'],
+  featureImg: './bg-dash.png',
+  projectDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+  seeLive: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+  seeSource: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+}, {
+  projectName: 'Website Portfolio',
+  projectLang: ['html', 'bootstrap', 'Ruby'],
+  featureImg: './bg-web.png',
+  projectDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+  seeLive: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+  seeSource: 'https://taiwoenoch.github.io/TaiwoEnoch-Portfolio/',
+}];
